24fc7d0c032697bcb65bba003e16d74745ff42fc
Nikolas <38259407+Nikolai558@users.noreply.github.com>
2020-04-21 22:09:43 -0600

    Functional VRC/Vatsim SCT Parser
    Functional VRC/Vatsim SCT Parser

--- SCT_PARSER.py
+++ SCT_PARSER.py
@@ -1,5 +1,5 @@
 import os
-
+import logging
 
 
 try:
@@ -24,34 +24,101 @@ with open("ZLC_SECTOR.SCT2", "r") as file:
     for lines in all_lines:
         current_line += 1
         next_line += 1
-        for character in lines[:7]:
+        for character in lines:
             if character[:1] == "#":
-                # put all of these in a file.
                 if os.path.exists(f"Sector_Files/{count_variable-1}_Colors.txt"):
                     color_file = open(f"Sector_Files/{count_variable-1}_Colors.txt", "a+")
                 else:
-                    #input("<<<<<want to create new file>>>>>>")
                     color_file = create_file("Sector_Files", count_variable, "Colors")
                 color_file.write(lines)
                 color_file.close()
+                print("Made it through # CHECK")
 
-            if character[:1] == "[":
+            elif character[:1] == "[" and lines[:5] != "[SID]" and lines[:6] != "[STAR]":
                 if os.path.exists(f"Sector_Files/{count_variable - 1}_{lines[:]}.txt"):
                     bracket_file = open(f"Sector_Files/{count_variable - 1}_{lines[:]}.txt", "a+")
                 else:
-                    #input("<<<<<want to create new file>>>>>>")
                     bracket_file = create_file("Sector_Files", count_variable, str(lines[:-1]))
                 bracket_file.write(lines)
                 for section in all_lines[current_line+1:]:
                     if section[:1] == "[":
                         bracket_file.write("\n")
                         bracket_file.close()
-                        #input("<<<<FOUND NEW BRACKET>>>>")
                         break
                     bracket_file.write(section)
-                if character[:5] == "[SID]":
-                    input("HEY YOU MADE IT TO THE SIDS")
+                print("Made it through bracket CHECK")
 
+            elif character[:1] == "[" and lines[:5] == "[SID]" and lines[:6] != "[STAR]":
+                global sid_name
+                if os.path.exists(f"Sector_Files/{count_variable - 1}_{lines[:]}.txt"):
+                    sids_file = open(f"Sector_Files/{count_variable - 1}_{lines[:]}.txt", "a+")
+                else:
+                    sids_file = create_file("Sector_Files", count_variable, str(lines[:-1]))
+                sids_file.write(lines)
+                sids_file.write("\n\n")
+                sids_file.close()
+                for section1 in all_lines[current_line+1:]:
+                    global certain_sid_file
+
+                    if section1[:1] != " " and section1[:1] != '\t' and section1[:1] != "\n" and section1[:1] != "[":
+                        sid_name = section1[:26].strip(" ")
+                        sid_name = sid_name.replace("/", "-")
+                        #print(sid_name)
+                        if os.path.exists(f"Sector_Files/{count_variable - 1}_{sid_name}.txt"):
+                            certain_sid_file = open(f"Sector_Files/{count_variable - 1}_{sid_name}.txt", "a+")
+                        else:
+                            certain_sid_file = create_file("Sector_Files", count_variable, sid_name)
+                        certain_sid_file.write(section1)
+
+                    elif section1[:1] == " " or section1[:1] == '\t' or section1[:1] == "\n":
+                        certain_sid_file.write(section1)
+
+                    elif section1[:1] == "[":
+                        certain_sid_file.write("\n\n")
+                        certain_sid_file.close()
+                        break
+                    else:
+                        pass
+                print("Made it through SID CHECK")
+                        #print("Came across something else: {}".format(section1[:25]))
+
+            elif character[:1] == "[" and lines[:6] == "[STAR]":
+                #input("<<<<<IN LAST IF STATEMENT>>")
+                global star_name
+
+                if os.path.exists(f"Sector_Files/{count_variable - 1}_{lines[:]}.txt"):
+                    star_file = open(f"Sector_Files/{count_variable - 1}_{lines[:]}.txt", "a+")
+                else:
+                    star_file = create_file("Sector_Files", count_variable, str(lines[:-1]))
+                star_file.write(lines)
+                star_file.write("\n\n")
+                star_file.close()
+                for section6 in all_lines[current_line+1:]:
+                    global certain_star_file
+
+                    if section6[:1] != " " and section6[:1] != '\t' and section6[:1] != "\n" and section6[:1] != "[":
+                        star_name = section6[:26].strip(" ")
+                        star_name = star_name.replace("/", "-")
+                        #print(star_name)
+                        if os.path.exists(f"Sector_Files/{count_variable - 1}_{star_name}.txt"):
+                            certain_star_file = open(f"Sector_Files/{count_variable - 1}_{star_name}.txt", "a+")
+                        else:
+                            certain_star_file = create_file("Sector_Files", count_variable, star_name)
+                        certain_star_file.write(section6)
+
+                    elif section6[:1] == " " or section6[:1] == '\t' or section6[:1] == "\n":
+                        certain_star_file.write(section6)
+
+                    elif section6[:1] == "[":
+                        certain_star_file.write("\n\n")
+                        certain_star_file.close()
+                        break
+                    else:
+                        pass
+                        print("Came across something else: {}".format(section6[:25]))
+                print("Made it through STAR CHECK")
+                #input("<<<<>>>>>")
+            #input("<<<<out of Star if statement>>>>")
 
 
 
