80591ef75738c6ebc835ddfd32b024fec222e050
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-07-29 18:32:29 -0600

    Program create folders

--- SCT_PARSER.py
+++ SCT_PARSER.py
@@ -3,8 +3,9 @@ import sys
 import urllib.request
 
 PROGRAM_VERSION = "2.0.1"
-GITHUB_VERSION = None
-file_counter = 10000
+GITHUB_VERSION = ""
+file_counter: int = 10000
+folder_counter: int = 1
 
 
 def check_version() -> None:
@@ -15,9 +16,9 @@ def check_version() -> None:
     """
     global GITHUB_VERSION
     global PROGRAM_VERSION
-    git_url = "https://raw.githubusercontent.com/Nikolai558/SCT_Parse/master/README.txt"
-    update_source = urllib.request.urlopen(git_url)
-    update_contents = update_source.readlines()
+    git_url: str = "https://raw.githubusercontent.com/Nikolai558/SCT_Parse/master/README.txt"
+    update_source: urllib.request.urlopen = urllib.request.urlopen(git_url)
+    update_contents: list = update_source.readlines()
     update_source.close()
 
     for i in update_contents:
@@ -28,9 +29,9 @@ def check_version() -> None:
         pass
     else:
         while True:
-            continue_var = input(f"Your Version: {PROGRAM_VERSION}\n"
-                                 f"Current Version on Github: {GITHUB_VERSION}\n"
-                                 f"Would you like to continue? [Y/N]: ").lower().strip()
+            continue_var: str = input(f"Your Version: {PROGRAM_VERSION}\n"
+                                      f"Current Version on Github: {GITHUB_VERSION}\n"
+                                      f"Would you like to continue? [Y/N]: ").lower().strip()
 
             if continue_var == "n":
                 exit()
@@ -39,18 +40,30 @@ def check_version() -> None:
                 return
 
 
-def create_folder() -> None:
+def create_folder(folder_name: str, directory: str = os.getcwd(), insert_number: bool = True) -> str:
     """
     Create's Folder to hold all of the split Sector Files.
 
-    :return: None
+    :param insert_number: Insert a number before the folder name
+    :param directory: Base Directory
+    :param folder_name: FolderName
+    :return: Full File Path for the Directory created
     """
+    global folder_counter
+
+    if insert_number:
+        output: str = f"{directory}\\{str(folder_counter).zfill(2)}_{folder_name}"
+        folder_counter += 1
+    else:
+        output: str = f"{directory}\\{folder_name}"
+
     try:
-        os.mkdir("Sector_Files")
-        print("Creating Folder for Sector_Files")
+        os.mkdir(output)
+        print(f"Creating Folder for {folder_name}")
     except FileExistsError:
-        print("Folder for Sector_Files Found\nPlease delete that folder or rename it.\nPress Enter to Close")
+        print(f"Folder for {folder_name} Found\nPlease delete that folder or rename it.\nPress Enter to Close")
         sys.exit()
+    return output
 
 
 def read_sector_file() -> list:
@@ -60,8 +73,8 @@ def read_sector_file() -> list:
     :return: List of strings.
     """
     try:
-        file = open("SECTOR.SCT2", "r")
-        output = file.readlines()
+        file: open = open("SECTOR.SCT2", "r")
+        output: list = file.readlines()
         file.close()
         print("Reading SECTOR.SCT2")
     except FileNotFoundError:
@@ -73,7 +86,7 @@ def read_sector_file() -> list:
 def create_individual_file(file_name: str, directory: str = "Sector_Files") -> open:
     global file_counter
     print(f"Creating File for {file_name}")
-    file = open(f"{directory}\\{file_counter}_{file_name}.txt", "w")
+    file: open = open(f"{directory}\\{file_counter}_{file_name}.txt", "w")
     file_counter += 10
     return file
 
@@ -85,29 +98,29 @@ def parse_sections(lines: list) -> dict:
     :param lines: List of strings (Lines in SCT2 file)
     :return: Dictionary, Key = Required Section, Value = List of strings for that Required Section
     """
-    output = {"STARTING": [], "COLORS": [], "INFO": [], "VOR": [], "NDB": [], "AIRPORT": [], "RUNWAY": [], "FIXES": [],
-              "ARTCC": [], "ARTCC HIGH": [], "ARTCC LOW": [], "SID": [], "STAR": [], "LOW AIRWAY": [],
-              "HIGH AIRWAY": [], "GEO": [], "REGIONS": [], "LABELS": []}
+    output: dict = {"STARTING": [], "COLORS": [], "INFO": [], "VOR": [], "NDB": [], "AIRPORT": [], "RUNWAY": [],
+                    "FIXES": [], "ARTCC": [], "ARTCC HIGH": [], "ARTCC LOW": [], "SID": [], "STAR": [],
+                    "LOW AIRWAY": [], "HIGH AIRWAY": [], "GEO": [], "REGIONS": [], "LABELS": []}
 
-    current_section = "STARTING"
+    current_section: str = "STARTING"
 
     for line in lines:
-        line = line.strip('\n')
+        line: str = line.strip('\n')
 
         if line == "":
-            line = "\n"
+            line: str = "\n"
 
         if current_section == "STARTING":
             if line[0] != "#":
                 output[current_section].append(line)
             else:
-                current_section = "COLORS"
+                current_section: str = "COLORS"
                 output[current_section].append(line)
             continue
 
         if current_section == "COLORS":
             if line[0] == "[":
-                current_section = line[1:-1]
+                current_section: str = line[1:-1]
                 output[current_section].append(line)
             else:
                 output[current_section].append(line)
@@ -115,14 +128,14 @@ def parse_sections(lines: list) -> dict:
 
         if current_section == "INFO":
             if line[0] == "[":
-                current_section = line[1:-1]
+                current_section: str = line[1:-1]
                 output[current_section].append(line)
             else:
                 output[current_section].append(line)
             continue
 
         if line[0] == "[":
-            current_section = line[1:-1]
+            current_section: str = line[1:-1]
             output[current_section].append(line)
         else:
             output[current_section].append(line)
@@ -138,17 +151,17 @@ def sub_section_helper(lines: list, sub_section: str) -> dict:
     :param lines: list of strings (All lines under the [SID] Section
     :return: Dictionary containing the required sub sections, Key = FileName (Alpha Numaric Safe), Value = List
     """
-    output = {}
+    output: dict = {}
 
     key_name = None
     for line in lines:
-        line = line.strip('\n')
+        line: str = line.strip('\n')
         if line == "":
-            line = "\n"
+            line:str = "\n"
 
         if line[0] == "[":
-            key_name = f"{sub_section}"
-            output[key_name] = []
+            key_name: str = f"{sub_section}"
+            output[key_name]: list = []
             output[key_name].append(line)
             continue
         if line[0] == ";":
@@ -158,18 +171,18 @@ def sub_section_helper(lines: list, sub_section: str) -> dict:
             output[key_name].append(line)
             continue
 
-        temp_name = line[:26].strip()
+        temp_name: str = line[:26].strip()
         if temp_name.isalnum():
-            key_name = temp_name
+            key_name: str = temp_name
         else:
-            key_name = ""
+            key_name: str = ""
             for c in temp_name:
                 if c.isalnum():
                     key_name += c
                 else:
                     key_name += "-"
 
-        output[key_name] = []
+        output[key_name]: list = []
         output[key_name].append(line)
 
     return output
@@ -184,16 +197,18 @@ def split_individual_sections(name: str, lines: list) -> None:
     :return: None
     """
     if name == "SID" or name == "STAR":
-        data = sub_section_helper(lines, name)
+        _dir: str = create_folder(directory=f"{os.getcwd()}\\Sector_Files", folder_name=name)
+        data: dict = sub_section_helper(lines, name)
         for k, v in data.items():
-            file = create_individual_file(k)
+            file: open = create_individual_file(k, directory=_dir)
             for line in v:
                 file.write(line + "\n")
             file.close()
         return
 
     else:
-        file = create_individual_file(name)
+        _dir: str = create_folder(directory=f"{os.getcwd()}\\Sector_Files", folder_name=name)
+        file: open = create_individual_file(name, directory=_dir)
         for line in lines:
             file.write(line + "\n")
         file.close()
@@ -205,11 +220,10 @@ def main() -> None:
 
     :return: None
     """
-    all_lines = read_sector_file()
+    all_lines: list = read_sector_file()
+    create_folder("Sector_Files", insert_number=False)
 
-    create_folder()
-
-    sections = parse_sections(all_lines)
+    sections: dict = parse_sections(all_lines)
 
     for k, v in sections.items():
         split_individual_sections(k, v)
